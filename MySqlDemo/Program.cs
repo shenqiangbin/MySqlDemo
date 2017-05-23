080b662886a9f00dc8c39e65d7958fe898c2bc5e
sqb <shenqiangbin@163.com>
2017-05-23 16:56:54 +0800

    add dll

--- MySqlDemo/Program.cs
+++ MySqlDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,25 @@ namespace MySqlDemo
     {
         static void Main(string[] args)
         {
+            string str = "server=127.0.0.1;database=ThesisMgmt;Uid=root;Pwd=123456;";//
+            str = "server=192.168.103.90;database=ThesisMgmt;Uid=root;Pwd=123456;";
+            using (MySqlConnection conn = new MySqlConnection(str))
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select getchildlist(1)";
+
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    Console.WriteLine();
+                }              
+            }
+
+            Console.WriteLine();
         }
     }
 }
