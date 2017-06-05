25c8f3a0048916313092a90e256bc2423d9dccfd
sqb <shenqiangbin@163.com>
2017-06-05 17:32:03 +0800

    add dapper mysql

---
+++ demo02/Program.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace demo02
+{
+    class Program
+    {
+        // 参考地址：http://www.cnblogs.com/Sinte-Beuve/p/4231053.html
+        static void Main(string[] args)
+        {
+            var str = "server=192.168.103.90;database=thesismgmt;Uid=thesismgmt;Pwd=123456;";
+            var conn = new MySqlConnection(str);
+            var list = conn.Query("SELECT * FROM thesismgmt.user;");
+
+            foreach (var item in list)
+            {
+                var s = item;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
